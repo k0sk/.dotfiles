9cc36a2ed045189f22b2be4baca24ad887df0ade
kosk <mail@kosk.me>
2016-12-27 15:00:55 +0900

    Renew setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+trap 'echo Error: $0: stopped; exit 1' ERR INT
 set -u
+set -e
 
-setup() {
-  dotfiles=$HOME/.dotfiles
-
-  has() {
-    type "$1" > /dev/null 2>&1
-  }
-
-  if [ -d "$dotfiles" ]; then
-    (cd "$dotfiles" && make update)
-  else
-    git clone https://github.com/k0sk/.dotfiles "$dotfiles"
-  fi
-
-  cd "$dotfiles" && has make && make install && make init
+has() {
+  type "$1" > /dev/null 2>&1
 }
 
-setup
+# Check if make and git are installed
+if [[ $OSTYPE != darwin* ]]; then
+  ! has make && echo "'make' has to be installed"
+  ! has git && echo "'git' has to be installed"
+else
+  # Install git through Xcode CLT
+  ! has git && echo "'git' has to be installed"; xcode-select --install
+fi
 
+dotfiles=$HOME/.dotfiles
+
+if [ ! -d "$dotfiles" ]; then
+  git clone https://github.com/k0sk/.dotfiles "$dotfiles"
+fi
+
+cd "$dotfiles"
+make install
